c8169f31fe63f4e4f4fd7c0675e89ac1ce21f716
Farshad Davoudi <f.davoudi.r@outlook.com>
2021-02-08 00:31:56 +0330

    Push test

--- Bit.WebSite/Startup.cs
+++ Bit.WebSite/Startup.cs
@@ -24,7 +24,7 @@ namespace Bit.WebSite
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
-            
+
             else
             {
                 app.UseExceptionHandler("/Error");
